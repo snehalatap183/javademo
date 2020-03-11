5926568da128a8e7a5f75303c8b0a6ba54d6ecf0
snehal patil <snehalatap183@gmail.com>
2020-03-11 14:20:06 -0700

    changes by developer commit

--- src/com/scp/java/gitdemo/Sample.java
+++ src/com/scp/java/gitdemo/Sample.java
@@ -1,6 +1,6 @@
 package com.scp.java.gitdemo;
 
-public class Sample {
+public class Sample D
 	
 public static void main(String[] args) {
 	
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	System.out.println("sample code suggested changes");
 	System.out.println("sample code changes added");
 	System.out.println("sample code clonned added another changes");
-	System.out.println("sample code clonned added another changes by developer1");
-	
+	System.out.println("sample code clonned added another changes by Developer1");
+
 }
 }

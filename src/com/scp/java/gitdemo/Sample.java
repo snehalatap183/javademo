4a5a6c0f5f4f9a06c8f59c45e015d040225521a1
snehal patil <snehalatap183@gmail.com>
2020-03-11 13:49:36 -0700

    another changes

--- src/com/scp/java/gitdemo/Sample.java
+++ src/com/scp/java/gitdemo/Sample.java
@@ -7,8 +7,7 @@ public static void main(String[] args) {
 	System.out.println("sample code clonned");
 	System.out.println("sample code suggested changes");
 	System.out.println("sample code changes added");
-	/*System.out.println("sample code clonned");
-	System.out.println("sample code clonned");*/
+	System.out.println("sample code clonned added another changes");
 	
 }
 }

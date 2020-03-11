faaf7ee42a2bddfd0db15982fcaf981cca1f89f3
snehal patil <snehalatap183@gmail.com>
2020-03-11 14:23:17 -0700

    final commit

--- src/com/scp/java/gitdemo/Sample.java
+++ src/com/scp/java/gitdemo/Sample.java
@@ -1,6 +1,6 @@
 package com.scp.java.gitdemo;
 
-public class Sample D
+public class Sample {
 	
 public static void main(String[] args) {
 	
@@ -8,12 +8,11 @@ public static void main(String[] args) {
 	System.out.println("sample code suggested changes");
 	System.out.println("sample code changes added");
 	System.out.println("sample code clonned added another changes");
-<<<<<<< HEAD
+
 	System.out.println("sample code clonned added another changes by Developer1");
 
-=======
+
 	System.out.println("sample code clonned added another changes by developer2");
 	
->>>>>>> branch 'master' of https://github.com/snehalatap183/javademo.git
 }
 }

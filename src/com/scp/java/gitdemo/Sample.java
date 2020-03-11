22fcd472577075e902c003e22fa9826ccc07fb20
snehal patil <snehalatap183@gmail.com>
2020-03-11 13:46:35 -0700

    changes added

--- src/com/scp/java/gitdemo/Sample.java
+++ src/com/scp/java/gitdemo/Sample.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	
 	System.out.println("sample code clonned");
 	System.out.println("sample code suggested changes");
-	//System.out.println("sample code changes added");
+	System.out.println("sample code changes added");
 	/*System.out.println("sample code clonned");
 	System.out.println("sample code clonned");*/
 	

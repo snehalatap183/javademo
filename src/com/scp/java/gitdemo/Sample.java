5cc8497a7ef39de685164e3bb29c323c15861031
snehalatap183 <58901588+snehalatap183@users.noreply.github.com>
2020-03-11 01:33:47 -0700

    Update Sample.java

--- src/com/scp/java/gitdemo/Sample.java
+++ src/com/scp/java/gitdemo/Sample.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	System.out.println("sample code suggested changes");
 	System.out.println("sample code changes added");
 	System.out.println("sample code clonned added another changes");
-	System.out.println("sample code clonned added another changes by dev2");
+	System.out.println("sample code clonned added another changes by developer2");
 	
 }
 }
